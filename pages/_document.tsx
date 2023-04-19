aa825c478f0cf60d0c6cecf4fb33075ff2058fa8
kubop <pernica.jakub@gmail.com>
2023-04-19 16:27:42 +0200

    Basic Layout

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></Script>
       </body>
     </Html>
   )
