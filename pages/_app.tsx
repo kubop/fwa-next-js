85caca6a9cbcee9b16b21ddf4977edd308d2a3e1
kubop <pernica.jakub@gmail.com>
2023-04-20 12:22:07 +0200

    Add NProgress bar and populate Users page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,30 @@
 import '@/styles/globals.css'
 import 'flowbite'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+
+import 'nprogress/nprogress.css'
+import NProgress from "nprogress";
 
 import Layout from '../components/Layout'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, router }: AppProps) {
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+ 
+    router.events.on("routeChangeStart", handleRouteStart);
+    router.events.on("routeChangeComplete", handleRouteDone);
+    router.events.on("routeChangeError", handleRouteDone);
+ 
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      router.events.off("routeChangeStart", handleRouteStart);
+      router.events.off("routeChangeComplete", handleRouteDone);
+      router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, [router.events])
+
   return (
     <Layout>
       <Component {...pageProps} />
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,8 +1,38 @@
-export default function Users() {
-    return (
-      <main className="flex flex-col items-center justify-between p-24">
-        Users
-      </main>
-    )
+import { InferGetServerSidePropsType } from "next"
+
+type Users = User[]
+
+type User = {
+  Id: number,
+  FirstName: string,
+  LastName: string,
+  Login: string,
+}
+
+export const getServerSideProps = async () => {
+  //const res = await fetch('https://www.bitstamp.net/api/v2/ticker/btcusd/') // TODO: Fetch users from backend
+  const users: Users = [
+    {
+      Id: 1,
+      FirstName: 'Jakub',
+      LastName: 'Pernica',
+      Login: 'kubop',
+    }
+  ]
+
+  await new Promise(resolve => setTimeout(resolve, 3000)); // Artificial delay
+
+  return {
+    props: {
+      users,
+    },
   }
-  
+}
+
+export default function Users({ users }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      {JSON.stringify(users)}
+    </main>
+  )
+}
