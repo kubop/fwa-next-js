9b5c754542a30710096be5dce7c6faf0b6587ef6
kubop <pernica.jakub@gmail.com>
2023-04-21 08:32:16 +0200

    Display table of addresses

--- pages/addresses/index.tsx
+++ pages/addresses/index.tsx
@@ -1,8 +1,87 @@
-export default function Addresses() {
-    return (
-      <main className="flex flex-col items-center justify-between p-24">
-        Addresses
-      </main>
-    )
+import Link from 'next/link'
+import { InferGetServerSidePropsType } from "next"
+
+type Addresses = Address[]
+
+type Address = {
+  "countUsers": number,
+  "addressId": number,
+  "street": string,
+  "number": number,
+  "city": string,
+  "zipCode": string
+}
+
+export const getServerSideProps = async () => {
+  const res = await fetch('https://127.0.0.1:7005/api/Address')
+  const addresses: Addresses = await res.json()
+
+  return {
+    props: {
+      addresses,
+    },
   }
+}
+
+export default function Addresses({ addresses }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <main className="flex items-center justify-center p-12">
+      <div className="shadow-md sm:rounded-lg overflow-auto">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" className="px-6 py-3">
+                        Street
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Number
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Zip Code
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        City
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        # of Users
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Actions
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+              {addresses.map(address => 
+                  <tr key={address.addressId} className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <td className="px-6 py-4">
+                      {address.street}
+                    </td>
+                    <td className="px-6 py-4">
+                      {address.number}
+                    </td>
+                    <td className="px-6 py-4">
+                      {address.zipCode}
+                    </td>
+                    <td className="px-6 py-4">
+                      {address.city}
+                    </td>
+                    <td className="px-6 py-4">
+                      {address.countUsers}
+                    </td>
+                    <td className="px-6 py-4">
+                        <Link href={`/address/${address.addressId}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                          Edit
+                        </Link>
+                        <Link href={`/address/delete/${address.addressId}`} className="ml-3 font-medium text-red-600 dark:text-red-500 hover:underline">
+                          Delete
+                        </Link>
+                    </td>
+                  </tr>
+              )}
+            </tbody>
+        </table>
+      </div>
+    </main>
+  )
+}
   
