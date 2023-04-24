08c698d104fc307d8b0d821372ef5860f9f1990e
kubop <pernica.jakub@gmail.com>
2023-04-24 09:35:18 +0200

    Add delete user functionality

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link'
 import { GetServerSideProps, InferGetServerSidePropsType } from "next"
 import { User } from '../../ts-types/types'
+import { useEffect, useState } from 'react'
 
-export const getServerSideProps: GetServerSideProps<{users: User[]}> = async () => {
+export const getServerSideProps: GetServerSideProps<{users: User[]}> = async () => { console.log('fetching users')
   const res = await fetch('https://127.0.0.1:7005/api/User')
   const users: User[] = await res.json()
 
@@ -14,6 +15,32 @@ export const getServerSideProps: GetServerSideProps<{users: User[]}> = async () 
 }
 
 export default function Users({ users }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [tableUsers, setTableUsers] = useState(users)
+  const [showDeletePopup, setShowDeletePopup] = useState<number | null>(null)
+
+  function handleDeleteButtonClicked(e: React.MouseEvent<HTMLButtonElement | MouseEvent>, userId: number) {
+    if (userId === showDeletePopup) {
+      fetch(`https://127.0.0.1:7005/api/user/${userId}`,{
+        method: 'DELETE',
+        body: JSON.stringify({
+          id: userId
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then((res) => {
+        if (res.ok) {
+          const updatedUsers = tableUsers.filter(u => u.userId !== userId)
+          setTableUsers(updatedUsers)
+        } else {
+          console.log('Handle error delete used popup')
+        }
+      })
+    }
+    
+    setShowDeletePopup(userId)
+  }
+
   return (
     <main className="flex items-center justify-center p-12">
       
@@ -39,7 +66,7 @@ export default function Users({ users }: InferGetServerSidePropsType<typeof getS
                   </tr>
               </thead>
               <tbody>
-                {users.map(user => 
+                {tableUsers.map(user => 
                     <tr key={user.userId} className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
                       <td className="px-6 py-4">
                           {user.firstName}
@@ -53,13 +80,22 @@ export default function Users({ users }: InferGetServerSidePropsType<typeof getS
                       <td className="px-6 py-4">
                         {user.address}
                       </td>
-                      <td className="px-6 py-4">
+                      <td className="px-6 py-4 flex">
                           <Link href={`/users/${user.userId}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
                             Edit
                           </Link>
-                          <Link href={`/users/delete/${user.userId}`} className="ml-3 font-medium text-red-600 dark:text-red-500 hover:underline">
-                            Delete
-                          </Link>
+                          {showDeletePopup === user.userId ? (
+                            <div className="flex">
+                              <button onClick={(e) => handleDeleteButtonClicked(e, user.userId)} className="w-max ml-3 font-medium text-red-600 dark:text-red-500 hover:underline">
+                                  Confirm delete
+                              </button>
+                              <button onClick={() => setShowDeletePopup(null)} className="w-max ml-2 font-medium text-red-600 dark:text-red-500 hover:underline">Undo</button>
+                            </div>
+                            ) : (
+                              <button onClick={() => setShowDeletePopup(user.userId)} className="w-max ml-3 font-medium text-red-600 dark:text-red-500 hover:underline">
+                                Delete
+                            </button>
+                            )}
                       </td>
                     </tr>
                 )}
