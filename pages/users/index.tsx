0a36f68a2feb7b16608ce646b40eae9f0fce52b2
kubop <pernica.jakub@gmail.com>
2023-04-24 10:36:23 +0200

    Implement sorting table by cols

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -2,9 +2,10 @@ import Link from 'next/link'
 import { GetServerSideProps, InferGetServerSidePropsType } from "next"
 import { User } from '../../ts-types/types'
 import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
-export const getServerSideProps: GetServerSideProps<{users: User[]}> = async () => { console.log('fetching users')
-  const res = await fetch('https://127.0.0.1:7005/api/User')
+export const getServerSideProps: GetServerSideProps<{users: User[]}> = async (context) => {
+  const res = await fetch(`https://127.0.0.1:7005/api/User?orderBy=${context.query?.orderBy}`)
   const users: User[] = await res.json()
 
   return {
@@ -14,10 +15,10 @@ export const getServerSideProps: GetServerSideProps<{users: User[]}> = async () 
   }
 }
 
-export default function Users({ users }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Users({ users }: InferGetServerSidePropsType<typeof getServerSideProps>) { 
+  const router = useRouter()
   const [tableUsers, setTableUsers] = useState(users)
   const [showDeletePopup, setShowDeletePopup] = useState<number | null>(null)
-
   function handleDeleteButtonClicked(e: React.MouseEvent<HTMLButtonElement | MouseEvent>, userId: number) {
     if (userId === showDeletePopup) {
       fetch(`https://127.0.0.1:7005/api/user/${userId}`,{
@@ -41,6 +42,26 @@ export default function Users({ users }: InferGetServerSidePropsType<typeof getS
     setShowDeletePopup(userId)
   }
 
+  function handleSortByClick(e: any, newSortBy: string) {
+    let oldSortBy = router.query?.orderBy
+
+    if (oldSortBy === newSortBy + `_DESC`) {
+      newSortBy = ""
+    }
+    else if (oldSortBy === newSortBy) {
+      newSortBy = newSortBy + `_DESC`
+    }
+
+    router.push({
+      pathname: router.pathname,
+      query: { orderBy: newSortBy }
+    })
+  }
+
+  useEffect(() => {
+    setTableUsers(users)
+  }, [users])
+
   return (
     <main className="flex items-center justify-center p-12">
       
@@ -48,16 +69,16 @@ export default function Users({ users }: InferGetServerSidePropsType<typeof getS
           <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
-                      <th scope="col" className="px-6 py-3">
+                      <th scope="col" className="px-6 py-3 hover:underline cursor-pointer" onClick={(e) => handleSortByClick(e, "FirstName")}>
                           First name
                       </th>
-                      <th scope="col" className="px-6 py-3">
+                      <th scope="col" className="px-6 py-3 hover:underline cursor-pointer" onClick={(e) => handleSortByClick(e, "LastName")}>
                           Last name
                       </th>
-                      <th scope="col" className="px-6 py-3">
+                      <th scope="col" className="px-6 py-3 hover:underline cursor-pointer" onClick={(e) => handleSortByClick(e, "Login")}>
                           Login
                       </th>
-                      <th scope="col" className="px-6 py-3">
+                      <th scope="col" className="px-6 py-3 hover:underline cursor-pointer" onClick={(e) => handleSortByClick(e, "Address")}>
                           Address
                       </th>
                       <th scope="col" className="px-6 py-3">
