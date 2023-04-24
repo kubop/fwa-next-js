85bd1c35cfe4b38cc87a5aaeb4f97127a7c07c4b
kubop <pernica.jakub@gmail.com>
2023-04-24 11:13:54 +0200

    Simple landing page + adjust css bg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,39 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      Hello world
+    <main className="font-mono flex flex-col items-center justify-between p-24">
+      <h1 className="font-bold text-6xl">Welcome to FWA</h1>
+      <span>(my <i>first web application</i>)</span>
+
+      <div className="mt-5">
+        <h2 className="font-semibold text-3xl">Users:</h2>
+        <ul>
+          <li>✅ Full view with address</li>
+          <li>✅ Fully implemented Edit</li>
+          <li>✅ Fully implemented Delete with Confirmation</li>
+          <li>✅ Fully implemented sorting by ALL columns</li>
+        </ul>
+      </div>
+
+      <div className="mt-5">
+        <h2 className="font-semibold text-3xl">Addresses:</h2>
+        <ul>
+          <li>✅ Full view with count of users</li>
+          <li>❌ <s>Fully implemented Edit</s></li>
+          <li>❌ <s>Fully implemented Delete with Confirmation</s></li>
+          <li>❌ <s>Fully implemented sorting by ALL columns</s></li>
+        </ul>
+      </div>
+
+      <div className="flex flex-col items-center mt-8">
+        <h2 className="font-semibold text-3xl">Powered by</h2>
+        <ul className="flex flex-col items-center mt-1">
+          <li>Next.js 13</li>
+          <li>ASP.NET Core Web API</li>
+          <li>Typescript</li>
+          <li>Tailwindcss</li>
+          <li>Flowbite</li>
+        </ul>
+      </div>
     </main>
   )
 }
