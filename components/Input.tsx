1e1942bbdb7dcda7240e81cfe64949a1f3e7e31c
kubop <pernica.jakub@gmail.com>
2023-04-26 10:28:48 +0200

    Create Input and Select components

---
+++ components/Input.tsx
@@ -0,0 +1,23 @@
+interface InputProps {
+    type?: string,
+    name: string,
+    label: string,
+    value: string | string[],
+    required?: boolean,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+}
+
+export default function Input({ name, label, type = "text", value, required, onChange }: InputProps) {
+    return (
+        <>
+            <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{ label }</label>
+            <input 
+                type={type}
+                name={name} 
+                onChange={(e) => onChange(e)} 
+                value={value} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                {...(required ? { required: true } : {})}
+            />
+        </>
+    )
+}
---
+++ components/Select.tsx
@@ -0,0 +1,25 @@
+interface SelectProps {
+    name: string,
+    label: string,
+    values: { id: number, value: string }[],
+    defaultValue: string,
+    onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void,
+}
+
+export default function Select({ name, label, values, defaultValue, onChange}: SelectProps) {
+    return (
+        <>
+            <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
+            <select name={name} defaultValue={defaultValue} onChange={onChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                {values.map(v => 
+                    <option
+                        key={v.id}
+                        value={v.id}
+                    >
+                        {v.value}
+                    </option>
+                )}
+            </select>
+        </>
+    )
+}
